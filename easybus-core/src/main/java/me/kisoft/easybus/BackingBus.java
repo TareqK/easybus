0189e921945054a4d00c0e5290c7a50ed770cb09
Tareq Kirresh <kirreshtareq@gmail.com>
2023-12-20 12:45:31 +0200

    added better logs

--- easybus-core/src/main/java/me/kisoft/easybus/BackingBus.java
+++ easybus-core/src/main/java/me/kisoft/easybus/BackingBus.java
@@ -1,10 +1,17 @@
 package me.kisoft.easybus;
 
+import java.util.Objects;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author tareq
  */
 public abstract class BackingBus implements AutoCloseable {
+
+    protected static final Logger log = LoggerFactory.getLogger(EasyBus.class);
 
     /**
      * Posts an object to the backing bus.
@@ -35,8 +42,21 @@ public abstract class BackingBus implements AutoCloseable {
      * @param event the event data
      * @param listener the listener to use
      */
-    protected <T extends Object> void handle(T event, Listener<T> listener) {
-        listener.on(event);
+    protected final <T extends Object> void handle(T event, Listener<T> listener) {
+        if (Objects.isNull(event)) {
+            log.warn("Received Null Event - skipping");
+            throw new IllegalArgumentException("Event Cannot be null");
+        }
+        if (Objects.isNull(listener)) {
+            log.warn("Received Null Listener - skipping ");
+            throw new IllegalArgumentException("Listener Cannot be null");
+        }
+        try {
+            listener.on(event);
+        } catch (Exception ex) {
+            log.info("Failed to Process Event {} : {} : {} caused by : {}", event.getClass().getCanonicalName(), ex.getClass().getCanonicalName(), ex.getMessage(), Optional.ofNullable(ex.getCause()).map(cause -> cause.getClass().getCanonicalName()).orElse("Unknown"));
+            throw new RuntimeException(ex);
+        }
     }
 
     @Override
